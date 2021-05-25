183ea07d23c6ab81530e5e82067d4dda0599de99
RodrigoRabellino <30850683+RodrigoRabellino@users.noreply.github.com>
2021-05-25 20:21:54 -0300

    frontend

---
+++ src/components/restaurantCard.js
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  makeStyles,
+  Card,
+  Button,
+  Typography,
+  CardContent,
+  CardActions,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const RestaurantCard = ({ restaurant, add: address }) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root} variant="elevation" elevation={4}>
+      <CardContent>
+        <Typography className={classes.title} variant="h4" >
+          {restaurant.name}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Cuisine: {restaurant.cuisine}
+        </Typography>
+        <Typography variant="h6">Address: {address}</Typography>
+      </CardContent>
+      <CardActions className={classes.buttonConainer}>
+      <Link to={"/restaurants/"+restaurant._id} className="btn btn-primary col-lg-5 mx-1 mb-1">
+                    View Reviews
+                  </Link>
+        
+
+        <Button
+          variant="contained"
+          color="primary"
+          href={`https://www.google.com/maps/place/${address}`}
+          target="_blank"
+        >
+          View Maps
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    margin: 4,
+    background: "#3DDBC1"
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    textAlign: "center",
+  },
+  pos: {
+    marginTop:12,
+    marginBottom: 12,
+  },
+  buttonConainer: {
+    justifyContent: "center",
+  },
+});
+
+export default RestaurantCard;
---
+++ src/services/restaurant.js
@@ -0,0 +1,31 @@
+import http from "../http-common";
+
+class RestaurantDataService {
+  //restaurants
+  getAll(page = 0) {
+    return http.get(`restaurants?page=${page}`);
+  }
+  get(id) {
+    return http.get(`restaurants?id=${id}`);
+  }
+
+  find(query, by = "name", page = 0) {
+    return http.get(`?${by}=${query}&page=${page}`);
+  }
+  //review
+  createReview(data) {
+    return http.post("review-new", data);
+  }
+  updateReview(data) {
+    return http.put("review-edit", data);
+  }
+  deleteReview(id, userId) {
+    return http.delete(`/review-delete?id=${id}`, { data: { userId: userId } });
+  }
+  //cuisines
+  getCuisines() {
+    return http.get(`/restaurants/cuisines`);
+  }
+}
+
+export default new RestaurantDataService();
